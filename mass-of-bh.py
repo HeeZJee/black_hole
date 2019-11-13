9af559861e701f6cbf141b62f493bd99eb67a3de
HeeZJee <hafeezghanchi927@gmail.com>
2019-11-14 01:26:14 +0500

    mass of black hole v3

--- mass-of-bh.py
+++ mass-of-bh.py
@@ -1,6 +1,7 @@
 import scipy.constants as sc
 from decimal import Decimal as dec
 
+
 _pi = sc.pi             #pi
 pi = dec(_pi)
 
@@ -13,32 +14,32 @@ G = dec(_G)
 
 d_unit = input('which unit you will like to you use \nAU or meters?\n')
 
-if d_unit == 'au' or unit == 'AU':
-    r_peri = dec( input("Enter radius at peristron: ")) #120
-    r_appi = dec( input("Enter radius at apustron: ")) #1800
-    r_peri_AU = dec(r_peri*149597900000)
-    r_appi_AU = dec(r_appi*149597900000)
-elif unit  == 'meters' or unit == 'meter' or unit == 'm' :
+if d_unit == 'au' or d_unit == 'AU':
+    r_periAU = dec( input("Enter radius at peristron: ")) #120
+    r_appiAU = dec( input("Enter radius at apustron: ")) #1800
+    r_appi = dec(r_appiAU*149597900000)
+    r_peri = dec(r_periAU*149597900000)
+elif d_unit  == 'meters' or d_unit == 'meter' or d_unit == 'm' :
     r_appi = dec( input("Enter radius at apustron: "))
     r_peri = dec( input("Enter radius at periastron: "))
 else:
     print("Invalid unit") 
+
 #t_unit = input('Enter unit for time period')
 #if t_unit == 'years' or t == 'year' or t == 'yrs' or t == 'yr':
-    t_yr = dec( input("Enter orbital period: ")) #491961600
-    t = dec(t_yr*dec(sc.year))
+t_yr = dec( input("Enter orbital period: ")) #491961600
+t = dec(t_yr*dec(sc.year))
 
-print("time is ",t)
 
-m_axis =  dec(pow(r_peri,3)+ 3*(pow(r_peri,2))*r_appi+ 3*r_peri*(pow(r_appi,2))+ pow(r_appi,3))
-t_sqr = pow(t,2)
-mass1= pow(pi,2)*pow(m_axis,3)
-mass2 = 2*G*t_sqr
-mass = mass1/mass2
+
+
+mass1= pow(pi,2)*pow(r_appi+r_peri,3)
+mass2 = 2*G*pow(t,2)
+mass = dec(mass1/mass2)
 
 print("mass is of object is %s kg" %mass)
 
 m_sun = 1.9889200011445836e30
 mass_of_sun = dec(m_sun)
-solar_mass = mass/mass_of_sun
+solar_mass = dec(mass/mass_of_sun)
 print('Its equal to %s solar masses.'%solar_mass)
